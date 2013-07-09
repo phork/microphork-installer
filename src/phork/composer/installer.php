508fa3d052c1f45065798327b145f5e8b153e933
Elenor <github@elenor.net>
2013-07-08 20:16:27 -0700

    Unindented installer file because indentation causes fatal errors

--- src/phork/composer/installer.php
+++ src/phork/composer/installer.php
@@ -1,30 +1,32 @@
 <?php
-	namespace Phork\Composer;
+namespace Phork\Composer;
 
-	use Composer\Package\PackageInterface;
-	use Composer\Installer\LibraryInstaller;
-	
-	class Installer extends LibraryInstaller {
-		
-		/**
-		 * {@inheritDoc}
-		 */
-		protected function getPackageBasePath(PackageInterface $package) {
-			if (preg_match('|microphork/package-([-\w]+)|', $package->getPrettyName(), $matches)) {
-				$name = $matches[1];
-			} else {
-				throw new \InvalidArgumentException('Unable to install package. The package name must start with "microphork/package-".');
-			}
-		
-			$this->initializeVendorDir();
-			return ($this->vendorDir ? $this->vendorDir.'/' : '').'microphork/packages/'.$name;
-		}
-	
-		
-		/**
-		 * {@inheritDoc}
-		 */
-		public function supports($packageType) {
-			return $packageType === 'microphork-package';
-		}
-	}
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+//this cannot have a space in front of it or InstallerInstaller::registerInstaller will fail
+class Installer extends LibraryInstaller 
+{
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected function getPackageBasePath(PackageInterface $package) {
+        if (preg_match('|microphork/package-([-\w]+)|', $package->getPrettyName(), $matches)) {
+            $name = $matches[1];
+        } else {
+            throw new \InvalidArgumentException('Unable to install package. The package name must start with "microphork/package-".');
+        }
+    
+        $this->initializeVendorDir();
+        return ($this->vendorDir ? $this->vendorDir.'/' : '').'microphork/packages/'.$name;
+    }
+
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType) {
+        return $packageType === 'microphork-package';
+    }
+}
